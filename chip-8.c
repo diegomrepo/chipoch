480866d308df8afe3c06bd7885d392dbb6f9e328
Diego Personal <diegomrepo@gmail.com>
2020-04-12 04:35:01 +0200

    Add support for Cycle and halt

--- chip-8.c
+++ chip-8.c
@@ -1,23 +1,22 @@
+#include "chip-8.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "chip-8.h"
 
+uint8_t chip_rand() { return rand() % 256; }
 
-#define START_ADD 0x200 /* Where programs start in RAM */
-#define TOTAL_RAM 4096
-#define RAM_LIMIT 0xE8F /* RAM limit for user programs */
+chp8_t *init_chip() {
+    printf("rand initialized: %d\n", chip_rand());
+    chp8_t *c8 = malloc(sizeof(chp8_t));
+    memset(c8, 0, sizeof(chp8_t));
+    memset(c8->memory, 0, MEM_SIZE);         /* best way? */
+    memset(c8->video, 0, sizeof(c8->video)); /* best way? */
+    memset(c8->V, 0, sizeof(c8->V)); /* TODO: Find out if this is best way */
+    c8->i_register = 0;
+    c8->pc = START_ADDR;
+    c8->sp = 0;
 
-uint8_t chip_rand()
-{
-    return rand() % 256;
-}
-
-chp8_t * init_chip() {
-  printf("rand initialized: %d\n", chip_rand());
-  chp8_t  *c8 = malloc(sizeof(chp8_t));
-  memset(c8, 0, sizeof(chp8_t));
-  printf("chip initialized\n");
-  return c8;
+    printf("chip initialized\n");
+    return c8;
 }
--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <time.h>
 
 #include "chip-8.h"
+/*#include "video.h" TODO: Move SDL */
 
 void clr(SDL_Renderer *renderer) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
@@ -14,12 +15,14 @@ void clr(SDL_Renderer *renderer) {
 }
 
 void init_video(SDL_Window **win, SDL_Renderer **renderer) {
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         exit(1);
     }
-    if (SDL_CreateWindowAndRenderer(640, 320, SDL_WINDOW_RESIZABLE, win, renderer)) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
+    if (SDL_CreateWindowAndRenderer(640, 320, SDL_WINDOW_RESIZABLE, win,
+                                    renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                     "Couldn't create window and renderer: %s", SDL_GetError());
         exit(3);
     }
     atexit(SDL_Quit);
@@ -55,17 +58,19 @@ void draw_pixel(SDL_Renderer *renderer, SDL_Rect *rect) {
     SDL_RenderPresent(renderer);
 }
 
- void init_rand() {
+void init_rand() {
     /* Intializes random number generator */
     time_t t;
-    srand((unsigned) time(&t));
- }
- void load_rom(chp8_t * c8, const char *rom) {
+    srand((unsigned)time(&t));
+}
+
+void load_rom(chp8_t *c8, const char *rom) {
     FILE *pFile;
-    unsigned char buffer[4096] = {0};
+    // unsigned char buffer[4096] = {0};
+    uint8_t *buffer = &(c8->memory[START_ADDR]);
     long lSize, result;
 
-    //const char *rom_path = "roms3/fishie.ch8";
+    // const char *rom_path = "roms3/fishie.ch8";
 
     pFile = fopen(rom, "rb");
     if (pFile == NULL) {
@@ -79,6 +84,8 @@ void draw_pixel(SDL_Renderer *renderer, SDL_Rect *rect) {
     fseek(pFile, 0, SEEK_END);
     lSize = ftell(pFile);
     rewind(pFile);
+    if (START_ADDR + lSize >= RAM_LIMIT)
+        exit(4);
 
     // copy the file into the buffer:
     result = fread(buffer, 1, lSize, pFile);
@@ -92,30 +99,59 @@ void draw_pixel(SDL_Renderer *renderer, SDL_Rect *rect) {
     /* the whole file is now loaded in the memory buffer. */
     printf("Size of buffer: %ld bytes \n", sizeof(buffer));
     for (int i = 0; i < lSize; i = i + 2)
-        printf("%02x%02x  ", buffer[i], buffer[i+1]);
+        printf("%02x%02x  ", buffer[i], buffer[i + 1]);
     putchar('\n');
+
     // terminate
     fclose(pFile);
- }
+}
+
+void dump_memory(chp8_t *c8) {
+    for (int i = 0; i < RAM_LIMIT; i = i + 2)
+        printf("%02x%02x  ", c8->memory[i], c8->memory[i + 1]);
+    putchar('\n');
+}
+
+void cycle(chp8_t *c8, bool *event) {
+
+    dump_memory(c8);
+    uint16_t opcode = 0;
+
+    printf("PC = 0x%04x\n", c8->pc);
+    opcode = c8->memory[c8->pc] << 8 | c8->memory[c8->pc + 1]; // get instruction
+    c8->pc = c8->pc + 2;
+    printf("opcode = 0x%04x\n", opcode);
+
+    int num = chip_rand();
+    putchar('\n');
+    printf("%d", num);
+    putchar('\n');
+    /*
+    if (num % 255 == 0)
+        *event = true;
+    SDL_Delay(100);
+    */
+    if (c8->pc > RAM_LIMIT)
+        *event = true;
+}
 
 int main() {
     SDL_Window *win;
     SDL_Renderer *renderer;
     SDL_Rect rect;
 
-    chp8_t * c8;
-    //const char *rom = "roms3/fishie.ch8";
+    chp8_t *c8;
+    // const char *rom = "roms3/fishie.ch8";
 
     init_rand();
     c8 = init_chip(); /*TODO: Load rom on init */
-    load_rom(c8, "../roms3/fishie.ch8");
+    load_rom(c8, "../roms2/RandomNumberTestMatthewMikolay2010.ch8");
     init_video(&win, &renderer);
-
     bool catched_event = false;
     while (!catched_event) {
-        draw_pixel(renderer, &rect); //SDL_Delay(1);
+        cycle(c8, &catched_event);
+        draw_pixel(renderer, &rect); // SDL_Delay(1);
         poll_events(&catched_event);
-        SDL_Delay(100);
     }
     free_video(renderer, win);
 }
